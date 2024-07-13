1e2d55a75af1d40198f04a5961bb85ba959632d4
Md Saiful Alam <82754863+saifulalam2559@users.noreply.github.com>
2024-07-13 20:36:30 +0200

    Create database.php

---
+++ database.php
@@ -0,0 +1,6 @@
+CREATE TABLE `impressions` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `ip_address` VARCHAR(50) NOT NULL,
+    `page_url` VARCHAR(255) NOT NULL
+);
